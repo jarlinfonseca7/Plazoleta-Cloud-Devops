f5689b9cf74e43e2b3692638c18c379313073325
Jarlin Fonseca <jarlin.fonsecab@pragma.com.co>
2023-07-08 11:43:48 -0500

    Probando trazas de x-ray

--- src/main/java/com/pragma/powerup/infrastructure/xray/WebConfig.java
+++ src/main/java/com/pragma/powerup/infrastructure/xray/WebConfig.java
@@ -1,12 +1,25 @@
 package com.pragma.powerup.infrastructure.xray;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.metrics.MetricsSegmentListener;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig {
+    static {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder
+                .standard()
+                .withPlugin(new EC2Plugin())
+                .withPlugin(new ElasticBeanstalkPlugin())
+                .withSegmentListener(new MetricsSegmentListener());
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
     @Bean
     public Filter TracingFilter() {
         return new AWSXRayServletFilter("plazoleta-service");
